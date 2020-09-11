86dc9994690ce2c9b8e5e7f7bbd0a4485f6accff
turinglabsorg <turinglabs@criptext.com>
2020-09-11 17:58:31 +0200

    renaming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const vm = require('./svm')
 const fs = require('fs')
-let code = fs.readFileSync('./modules/helloworld.js')
+let code = fs.readFileSync('./ida_modules/helloworld.js')
 
 async function runModule() {
     let result = await vm.run(code, 'helloworld', "turinglabs")
