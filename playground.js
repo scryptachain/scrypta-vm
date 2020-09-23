55d92e64ba1dd1f79adf4dc69dc332a260073e3a
turinglabsorg <turinglabs@criptext.com>
2020-09-23 18:04:12 +0200

    min fix

--- playground.js
+++ playground.js
@@ -15,8 +15,9 @@ async function runContract() {
     let id = await scrypta.createAddress('123456', false)
     scrypta.staticnodes = true
     scrypta.debug = true
-    let request = await scrypta.createContractRequest(id.walletstore, '123456', {contract: 'LcD7AGaY74xvVxDg3NkKjfP6QpG8Pmxpnu', function: 'search', params: {address: 'LchzGX6vqmanceCzNUMTk5cmnt1p6knGgT'}})
-    let result = await scrypta.sendContractRequest(request, 'https://idanodejs01.scryptachain.org')
+    let request = await scrypta.createContractRequest(id.walletstore, '123456', {contract: 'LcD7AGaY74xvVxDg3NkKjfP6QpG8Pmxpnu', function: 'search', params: {name: 'turinglabs'}})
+    let result = await scrypta.sendContractRequest(request, 'https://idanodejs02.scryptachain.org')
+    console.log(result)
 }
 //readContract()
 runContract()
